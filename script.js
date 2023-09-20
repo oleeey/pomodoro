30e2a6174f647e51e1c4cf9249cdbafc115753e9
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-09-20 22:12:52 +0200

    added structure, able to change break & work time,

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    let breakTime = 5;
+    let workTime = 20;
+
+    $(".adjustBreak").click(function(event) {
+        if (event.target.id == "minusBreak" && breakTime > 1) {
+            breakTime -= 1;
+        }
+        if (event.target.id == "plusBreak") {
+            breakTime += 1;
+        }
+        $("#breakDisplay").text(breakTime);
+    })
+
+    $(".adjustWork").click(function(event) {
+        if (event.target.id == "minusWork" && workTime > 1) {
+            workTime -= 1;
+        }
+        if (event.target.id == "plusWork") {
+            workTime += 1;
+        }
+        $("#workDisplay").text(workTime);
+    })
+
+
+
+});
