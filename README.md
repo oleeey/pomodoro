11c45a20a22d105786974936130c143f33f1197a
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-09-26 20:42:43 +0200

    fixed work and break length adjust, tried out different methods for toggling clock, not working yet

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # TODO:
 - hide start button after clicked
 - pause button
-- session length adjust fix
 - grafix
 
 
--- script.js
+++ script.js
@@ -5,40 +5,40 @@ $(document).ready(function() {
 
     $(".adjustBreak").click(function(event) {
         if (event.target.id == "minusBreak" && breakTime > 1) {
-            breakTime -= 1;
+            breakTime = parseInt(breakTime) - 1;
         }
         if (event.target.id == "plusBreak") {
-            breakTime += 1;
+            breakTime = parseInt(breakTime) + 1;
         }
         $("#breakDisplay").text(breakTime);
     })
 
     $(".adjustWork").click(function(event) {
         if (event.target.id == "minusWork" && workTime > 1) {
-            workTime -= 1;
+            workTime = parseInt(workTime) - 1;
         }
         if (event.target.id == "plusWork") {
-            workTime += 1;
+            workTime = parseInt(workTime) + 1;
         }
         $("#workDisplay").text(workTime);
+        $("#clockDisplay").text(workTime + ":00");
     })
 
     $(".btn").click(function(event) {
         switch(event.target.value) {
             case "start": 
-                clock(2, 1, "work");
+                clock(breakTime, workTime, "work");
         }
     })
 });
 
-function clock(breakTime, workTime, status, active = true) {
+function clock(breakTime, workTime, status) {
     status == "work" ? time = workTime : time = breakTime;
     let seconds = time * 60;
-    console.log(active);
 
     const x = setInterval(function() {
         if (seconds > 0) {
-            seconds -= 10;
+            seconds -= 1;
             let minutes = Math.floor(seconds / 60);
             console.log(minutes, seconds % 60);
             $("#clockDisplay").text(`${minutes}:${seconds % 60}`);
@@ -53,7 +53,6 @@ function clock(breakTime, workTime, status, active = true) {
         switch(event.target.value) {
             case "start":
                 clearInterval(x);
-                active = false;
         }
     }) 
 }
