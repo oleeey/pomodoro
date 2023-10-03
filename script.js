007dfdd6706770f19673b3538dda4ce33b447498
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-10-03 20:21:22 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 $(document).ready(function() {
     let breakTime = $("#breakDisplay").text();
     let workTime = $("#workDisplay").text();
-    let active = false;
+
+    var x;
+    let status = "work";
+
+    function loop() {
+        if (!x) {
+            x = setInterval(function() {
+                status == "work" ? time = workTime : time = breakTime;
+
+                let seconds = time * 60;
+
+                if (seconds > 0) {
+                    seconds -= 1;
+                    let minutes = Math.floor(seconds / 60);
+                    console.log(minutes, seconds % 60);
+                    $("#clockDisplay").text(`${minutes}:${seconds % 60}`);
+                }
+                else {
+                    clearInterval(x);
+                    time == workTime ? clock(breakTime, workTime, "break") : clock(breakTime, workTime, "work");
+                }
+            
+            }, 1000)
+        }
+        else {
+            clearInterval(x);
+            x = null;
+        }
+    }
 
     $(".adjustBreak").click(function(event) {
         if (event.target.id == "minusBreak" && breakTime > 1) {
@@ -27,40 +55,8 @@ $(document).ready(function() {
     $(".btn").click(function(event) {
         switch(event.target.value) {
             case "start": 
-                clock(breakTime, workTime, "work");
+                loop();
         }
     })
 });
 
-function clock(breakTime, workTime, status) {
-    status == "work" ? time = workTime : time = breakTime;
-    let seconds = time * 60;
-    let isPaused = false;
-    var x;
-
-
-    if (!isPaused) {
-        x = setInterval(function() {
-            if (seconds > 0) {
-                seconds -= 1;
-                let minutes = Math.floor(seconds / 60);
-                console.log(minutes, seconds % 60);
-                $("#clockDisplay").text(`${minutes}:${seconds % 60}`);
-            }
-            else {
-                clearInterval(x);
-                time == workTime ? clock(breakTime, workTime, "break") : clock(breakTime, workTime, "work");
-            }
-        }, 1000)
-    }
-
-    $(".btn").click(function(event) {
-        switch(event.target.value) {
-            case "start":
-                clearInterval(x);
-            case "pause":
-                clearInterval(x);
-                isPaused = true;
-        }
-    }) 
-}
